af79a751bafee91d8ad8856c33bb1dbb657f3aa2
Kashyap Patel <kvpatel95@gmail.com>
2023-05-10 01:36:49 -0400

    removed unused references

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import ReactStars from 'react-stars'
 import { useParams } from 'react-router-dom';
-import { doc, getDoc, getDocs } from 'firebase/firestore';
-import { moviesRef, reviewRef } from '../firebase/firebase';
+import { doc, getDoc } from 'firebase/firestore';
 import { db } from '../firebase/firebase';
 import Reviews from './Reviews';
 
